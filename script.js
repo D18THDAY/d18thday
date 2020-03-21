97b9013053e4f2f9721899f77276b0cb50a60c74
Seun Sofoluwe <39651850+D18THDAY@users.noreply.github.com>
2020-03-21 12:17:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let names = "Oluwaseun Sofoluwe Samuel";
+let courses = ["html", "css", "Javascript", "UI/UX"];
 
+
+console.log(`Names: ${names}, courses: ${courses}`);
+
+if(courses.length % 2===0){
+    for(counter = 1; counter <=200; counter++){
+        if(counter % 2 ===0){
+            console.log(counter)
+        }
+    }
+    
+}else{
+    for(counter = 1; counter <=200; counter++){
+        if(counter % 2 != 0){
+            console.log(counter)
+        }
+    }
+}
